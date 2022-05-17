b8c288ebf2a409af7a12817c06fae784878a7e7a
Lior <levydlior@gmail.com>
2022-05-16 20:08:31 -0600

    commit tree 97efd97cd293822c2f0e0208d57480e49ec025ba

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+ const textToReplace = document.querySelector("#text")
+ textToReplace.textContent = "This is really cool!"
+
+
+  });
